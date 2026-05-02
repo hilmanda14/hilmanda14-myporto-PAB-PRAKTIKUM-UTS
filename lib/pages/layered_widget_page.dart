823dff3ae681c4dcdc30f91499d96fdff767ee8b
hilmanda14 <hhilmanda@gmail.com>
2026-05-02 13:00:56 +0700

    Add files via upload

---
+++ lib/pages/layered_widget_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class LayeredWidgetPage extends StatelessWidget {
+  const LayeredWidgetPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Widget Bertingkat'),
+      ),
+      body: Center(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Container(
+              width: 200,
+              height: 200,
+              color: Colors.red,
+            ),
+            Container(
+              width: 150,
+              height: 150,
+              color: Colors.green,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue,
+              child: const Center(
+                child: Text(
+                  'Layered',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
