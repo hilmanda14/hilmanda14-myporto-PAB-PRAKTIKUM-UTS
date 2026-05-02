ba1eeda2223dddfa135cd79966090d25eca1bdd4
hilmanda14 <hhilmanda@gmail.com>
2026-05-02 13:54:14 +0700

    Add files via upload

---
+++ lib/pages/grid_view_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class GridViewPage extends StatelessWidget {
+  const GridViewPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Grid View'),
+      ),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(16.0),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+        itemCount: 9,
+        itemBuilder: (context, index) {
+          return Container(
+            color: Colors.primaries[index % Colors.primaries.length],
+            child: Center(
+              child: Text(
+                'Item ${index + 1}',
+                style: const TextStyle(color: Colors.white),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
